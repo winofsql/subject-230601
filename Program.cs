dc1f0021e305169cc4a8df93363779b316327212
sworc <lightbox@nifty.com>
2023-06-01 12:02:24 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.Odbc;
+using System.Diagnostics;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("こんにちは世界");
+            Debug.WriteLine("こんにちは世界");
+
+            OdbcConnectionStringBuilder builder = new OdbcConnectionStringBuilder();
+            builder.Driver = "MySQL ODBC 8.0 Unicode Driver";
+
+            // 接続用のパラメータを追加
+            builder.Add("server", "localhost");
+            builder.Add("database", "lightbox2");
+            builder.Add("uid", "root");
+            builder.Add("pwd", "");
+
+            // 接続の作成
+            OdbcConnection myCon = new OdbcConnection();
+
+            // MySQL の接続準備完了
+            myCon.ConnectionString = builder.ConnectionString;
+
+            // MySQL に接続
+            myCon.Open();
+
+            myCon.Close();
+        }
+    }
+}
